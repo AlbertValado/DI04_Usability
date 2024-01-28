a625e51630210deed0ce20d964960dc6e5b122ae
Albert Valado <albertovalado@paucasesnovescifp.cat>
2024-01-28 11:15:58 +0100

    added project from pevious task

---
+++ ValadoFitnessHack/src/main/java/avp/valadofitnesshack/dto/Review.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package avp.valadofitnesshack.dto;
+
+/**
+ *
+ * @author Albert
+ */
+public class Review {
+    private int id;
+    private int idIntent;
+    private int idReviewer;
+    private int valoracio;
+    private String comentari;
+
+    public Review() {
+    }
+
+    public Review(int id, int idIntent, int idReviewer, int valoracio, String comentari) {
+        this.id = id;
+        this.idIntent = idIntent;
+        this.idReviewer = idReviewer;
+        this.valoracio = valoracio;
+        this.comentari = comentari;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" + "id=" + id + ", idIntent=" + idIntent + ", idReviewer=" + idReviewer + ", valoracio=" + valoracio + ", comentari=" + comentari + '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdIntent() {
+        return idIntent;
+    }
+
+    public void setIdIntent(int idIntent) {
+        this.idIntent = idIntent;
+    }
+
+    public int getIdReviewer() {
+        return idReviewer;
+    }
+
+    public void setIdReviewer(int idReviewer) {
+        this.idReviewer = idReviewer;
+    }
+
+    public int getValoracio() {
+        return valoracio;
+    }
+
+    public void setValoracio(int valoracio) {
+        this.valoracio = valoracio;
+    }
+
+    public String getComentari() {
+        return comentari;
+    }
+
+    public void setComentari(String comentari) {
+        this.comentari = comentari;
+    }
+    
+    
+}
---
+++ ValadoFitnessHack/src/main/java/avp/valadofitnesshack/dto/Usuari.java
@@ -0,0 +1,68 @@
+
+package avp.valadofitnesshack.dto;
+
+/**
+ *
+ * @author Albert
+ */
+public class Usuari {
+    private int Id;
+    private String Nom;
+    private String Email;
+    private String PasswordHash;
+    private byte[] Foto;
+    private boolean Instructor;
+
+    @Override
+    public String toString(){
+        return Nom + "  " + Email + "  És instructor?:  " + Instructor;
+    }
+    
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String Nom) {
+        this.Nom = Nom;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPasswordHash() {
+        return PasswordHash;
+    }
+
+    public void setPasswordHash(String PasswordHash) {
+        this.PasswordHash = PasswordHash;
+    }
+
+    public byte[] getFoto() {
+        return Foto;
+    }
+
+    public void setFoto(byte[] Foto) {
+        this.Foto = Foto;
+    }
+
+    public boolean isInstructor() {
+        return Instructor;
+    }
+
+    public void setInstructor(boolean Instructor) {
+        this.Instructor = Instructor;
+    }
+}
